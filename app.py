3d720320c7f70eb2c1bcfffa0096798c6b9fc797
Farid Hasfindra <farid06hasfindra@gmail.com>
2025-11-19 18:20:25 +0700

    !

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Cat vs Dog Classifier", page_icon="🐾")
 st.title("🐱🐶 Cat vs Dog Classifier")
 
 # URL API inference Hugging Face (ubah sesuai modelmu)
-API_URL = "https://cat-dog-classification.asdjbfag.hf.space/predict"
+API_URL = "https://asdjbfag-cat-dog-classification.hf.space/predict"
 
 # Jika endpoint butuh autentikasi tambahkan token kamu
 headers = {"Authorization": "Bearer HF_API_KEY"}  # isi tokenmu
